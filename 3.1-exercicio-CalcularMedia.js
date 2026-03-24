d09cd50705a4ca963005c2b455c9a7e47276188b
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-03-24 17:39:41 -0300

    Arquivo de Calculo de Média

---
+++ 3.1-exercicio-CalcularMedia.js
@@ -0,0 +1,10 @@
+const notaUm = 10;
+var notaDois = 5;
+let notaTres = 10;
+var numeroDeNotas = 3;
+
+console.log((notaUm + notaDois + notaTres) / numeroDeNotas);
+
+notaDois = 8;
+
+console.log((notaUm + notaDois + notaTres)/ numeroDeNotas);
